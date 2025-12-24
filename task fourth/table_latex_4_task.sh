4c9c51e8f92cdd23c961b5d1c3e43468ea10ee2c
Vadim M <mv-sophos@yandex.ru>
2025-12-24 16:25:52 +0300

    Четвертая задача. Сама схема кажется верная, парни сказали что нужно делать так, и написан скрипт для фонового режима

---
+++ task fourth/table_latex_4_task.sh
@@ -0,0 +1,27 @@
+# вот как вызывать для параметров C=10 и mu = 0.001
+# table_latex_4_task.sh 10 0.001
+
+g++ fourth_part.cpp
+
+C=$1
+mu=$2
+filename="TABLE_C=$1_rho=$2.txt"
+
+echo -E "\begin{table}[htbp] \centering \begin{tabular}{|c * {7}{|c}|} \hline \multicolumn{8}{|c|}{$ C=$C, \; \mu = $mu$} \\\\ \hline \diagbox{$\rho$}{u} & 1 & 2 & 3 & 4 & 5 & 6 & 7 \\\\ \hline"                                                    >  $filename
+for rho in {1..7}
+do
+        for u in {1..7}
+        do
+                if [[ $u == 1 ]]
+                then
+                        echo -n "$rho "                                                         >> $filename
+                fi
+                echo -n "& $(echo -1 | ./a.out 1000 50000 0      $u $rho      $C $mu 10) "      >> $filename
+        done
+        echo -E "\\\\ \hline"                                                                   >> $filename
+done
+echo -E "\end{tabular}"                                                                         >> $filename
+echo -E "\end{table}"                                                                           >> $filename
+
+echo "для параметров C=$C и mu=$mu скрипт окончен"
+
